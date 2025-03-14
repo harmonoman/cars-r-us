0acebe5fad8afeb8c7f60a2d386d22b062afd2cd
Mark Bond <markbond615@gmail.com>
2025-03-13 20:19:47 -0500

    Refactored options dropdown modules to fetch from database.json

--- scripts/Technologies.js
+++ scripts/Technologies.js
@@ -1,13 +1,19 @@
 import { getTransientState, setTechnology } from "./transientState.js";
 
-export const DisplayTechnologyOptions = () => {
-    const html = `<select id="technology">
-        <option value="" disabled selected>Choose a technology package</option>
-        <option value="0">Basic Package</option>
-        <option value="1">Navigation Package</option>
-        <option value="2">Visibility Package</option>
-        <option value="3">Ultra Package</option>
-    </select>`;
+export const DisplayTechnologyOptions = async () => {
+    const response = await fetch("http://localhost:8088/technology");
+    const technologies = await response.json();
+
+    let html = `<h2>Technologies</h2>`;
+;   html += `<select id="technology">`;
+    html += `<option value="0">Select a technology package</option>`;
+
+    const arrayOfOptions = technologies.map( (technology) => {
+        return `<option value="${technology.id}">${technology.technology}</option>`;
+    })
+
+    html += arrayOfOptions.join("");
+    html += `</select>`;
 
     return html;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 import { DisplayWheelOptions } from "./wheels.js";
 import { DisplayPaintOptions } from "./Paints.js";
-import { DisplayInteriorOptions } from "./Interiors.js";
 import { DisplayTechnologyOptions } from "./Technologies.js";
-import { getTransientState } from "./transientState.js";
-
+import { DisplayInteriorOptions } from "./Interiors.js";
 
 const container = document.querySelector("#container");
 
 const optionRenderers = [DisplayInteriorOptions, DisplayPaintOptions, DisplayTechnologyOptions, DisplayWheelOptions];
 
-const render = () => {
+const render = async () => {
+    const dropdowns = await Promise.all(optionRenderers.map(fn => fn()));
+
     const html = `<div class="dropdown-container">
-    ${optionRenderers.map(fn => `<div class="dropdown">${fn()}</div>`).join("")}
+    ${dropdowns.map(dropdown => `<div class="dropdown">${dropdown}</div>`).join("")}
 </div>`;
     container.innerHTML = html;
 }
